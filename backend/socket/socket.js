20ed542dd8077562b5e891d7bd8eb236cdafcde7
UTKARSH SHARMA <75742292+UtkarshSharma1209@users.noreply.github.com>
2024-08-06 00:48:39 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,7 +1,8 @@
 import {Server} from "socket.io";
 import express from "express";
 import http from "http";
-
+import dotenv from "dotenv";
+dotenv.config({});
 const app = express();
 
 const server = http.createServer(app);
